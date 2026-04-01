d49433c6d379109fc6e4761c39a6584b49b074fa
Walter Bai <bwz2009ss@gmail.com>
2026-04-01 00:53:25 -0700

    Clean up platform dispatch in test data loader

--- test/test_data_stream_os_specific.cpp
+++ test/test_data_stream_os_specific.cpp
@@ -1,10 +1,8 @@
 #include "test_data_stream.h"
 
-#ifdef _WIN32
+#if defined(_WIN32)
 #include <windows.h>
-#endif
-
-#ifdef __APPLE__
+#elif defined(__APPLE__)
 #include <mach-o/getsect.h>
 #include <mach-o/loader.h>
 #include "resources.h"
@@ -13,15 +11,13 @@ extern const mach_header_64 _mh_execute_header;
 #else
 extern const mach_header _mh_execute_header;
 #endif
-#endif
-
-#ifdef __linux__
+#elif defined(__linux__)
 #include "resources.h"
 #endif
 
 const char* TestDataIStream::LoadResourceData(const std::string& resourceName, std::streamsize& dataSize)
 {
-#ifdef _WIN32
+#if defined(_WIN32)
     HMODULE handle = GetModuleHandle(NULL);
     HRSRC res = FindResource(handle, resourceName.c_str(), RT_RCDATA);
     if (!res)
@@ -37,9 +33,7 @@ const char* TestDataIStream::LoadResourceData(const std::string& resourceName, s
 
     dataSize = SizeofResource(handle, res);
     return static_cast<const char*>(LockResource(resHandle));
-#endif
-
-#ifdef __linux__
+#elif defined(__linux__)
     if (g_binaryDataMap.count(resourceName))
     {
         const auto &buf = g_binaryDataMap.at(resourceName);
@@ -48,9 +42,7 @@ const char* TestDataIStream::LoadResourceData(const std::string& resourceName, s
     }
 
     return nullptr;
-#endif
-
-#ifdef __APPLE__
+#elif defined(__APPLE__)
     const auto it = g_binarySectionMap.find(resourceName);
     if (it == g_binarySectionMap.end())
     {
@@ -58,11 +50,7 @@ const char* TestDataIStream::LoadResourceData(const std::string& resourceName, s
     }
 
     unsigned long size = 0;
-#if defined(__LP64__)
     const auto* data = getsectiondata(&_mh_execute_header, "__YUVTEST", it->second.c_str(), &size);
-#else
-    const auto* data = getsectiondata(&_mh_execute_header, "__YUVTEST", it->second.c_str(), &size);
-#endif
     if (!data)
     {
         return nullptr;
@@ -70,7 +58,7 @@ const char* TestDataIStream::LoadResourceData(const std::string& resourceName, s
 
     dataSize = static_cast<std::streamsize>(size);
     return reinterpret_cast<const char*>(data);
-#endif
-
+#else
     return nullptr;
+#endif
 }
